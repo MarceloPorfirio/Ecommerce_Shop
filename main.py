8eac4af7c0e45dc51832af451aaf2782cf7173e4
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2024-09-04 20:18:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import flet as ft
 
 
-class Landing(ft.view):
-    def __init__(self,page:ft.Page) -> None:
+class Landing(ft.View):
+    def __init__(self ,page: ft.Page) -> None:
         super(Landing,self).__init__(
             route="/",horizontal_alignment='center',
             vertical_alignment='center'
@@ -10,12 +10,27 @@ class Landing(ft.view):
 
         self.page = page
         self.cart_logo = ft.Icon(name=ft.icons.SHOPPING_CART_CHECKOUT_OUTLINED,size=64)
+        self.title = ft.Text('MINHA LOJA'.upper(),size=28, weight='bold')
+        self.subtitle = ft.Text('Desenvolvido com Flet', size=11)
         self.controls = [
-            self.cart_logo
+            self.cart_logo,
+            ft.Divider(height=25,color="transparent"),
+            self.title,
+            self.subtitle,
+            ft.Divider(height=10,color="transparent"),
         ]
 
 def main(page:ft.Page) -> None:
-    page.add(ft.SafeArea(ft.Text('Hello, Flet')))
+    def router(route) -> None:
+        page.views.clear()
 
+        if page.route == '/':
+            landing = Landing(page)
+            page.views.append(landing)
+
+        page.update()
+
+    page.on_route_change = router
+    page.go('/')
 
 ft.app(main)
